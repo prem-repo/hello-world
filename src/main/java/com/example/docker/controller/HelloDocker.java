f2d2794b2e2881158598aee1fb9159e94f442acc
prem-repo <rai.prem1230@gmail.com>
2020-04-12 19:30:52 +0530

    initial commit

---
+++ src/main/java/com/example/docker/controller/HelloDocker.java
@@ -0,0 +1,15 @@
+package com.example.docker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/docker/hello")
+public class HelloDocker {
+
+	@GetMapping
+	public String helloDocker(){
+		return "hello docker";
+	}
+}
